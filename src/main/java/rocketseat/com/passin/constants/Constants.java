5ec7407d6749a1b9e742b9b749427c1e68978b4d
João Victor Bezerra da Silva <dev.joaovictor@hotmail.com>
2024-04-02 12:38:56 +0100

    feat: add Constants

---
+++ src/main/java/rocketseat/com/passin/constants/Constants.java
@@ -0,0 +1,10 @@
+package rocketseat.com.passin.constants;
+
+public class Constants {
+    // Genericals
+    public static String SUCCESS = "Working Successful";
+
+    // EVENTS
+    public static String SUCCESSFUL_CREATED = "Event successful created";
+
+}
