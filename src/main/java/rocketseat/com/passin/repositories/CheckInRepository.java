46e6bd83362a666c72ce49c4b0297b0d563e5d37
João Victor Bezerra da Silva <dev.joaovictor@hotmail.com>
2024-04-02 17:15:35 +0100

    feat: add FindByAttendeeId method on CheckInRepository

--- src/main/java/rocketseat/com/passin/repositories/CheckInRepository.java
+++ src/main/java/rocketseat/com/passin/repositories/CheckInRepository.java
@@ -3,5 +3,8 @@ package rocketseat.com.passin.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import rocketseat.com.passin.domain.check_in.CheckIn;
 
+import java.util.Optional;
+
 public interface CheckInRepository extends JpaRepository<CheckIn, String> {
+    Optional<CheckIn> findByAttendeeId(String attendeeId);
 }
